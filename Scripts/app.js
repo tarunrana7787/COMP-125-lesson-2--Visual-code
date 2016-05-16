02d0b3870483ab33a6173505654ebd423042eb9e
tarunrana7787 <tarunrana7787@gmail.com>
2016-05-16 10:43:00 -0400

    Initial Commit

---
+++ Scripts/app.js
@@ -0,0 +1,20 @@
+/* main JavaScript file */
+
+//IIFE - Immediately Invoked Function Expression
+(function(){
+    "use strict";
+    
+    
+    
+    //code codes in here
+    console.log("App Started...");
+    
+    //create 
+    var firstParagraph = document.getElementById("firstparagraph");
+    
+    firstParagraph.textContent = "It's alive!!!";
+    
+    
+    
+    
+})();
